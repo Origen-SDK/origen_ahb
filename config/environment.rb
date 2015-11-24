6c5ae976c1120960a712f2eca7cda59183100601
Chris Nappi <chris.nappi@freescale.com>
2015-11-24 08:45:21 -0600

    Web mods -> Origen

--- config/environment.rb
+++
@@ -1,4 +0,0 @@
-# This file will be loaded by RGen to boot your application, just leave it as
-# is and modify lib/origen_ahb.rb to load the additional resources that your
-# application requires
-require "origen_ahb"
